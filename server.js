cd1a8585da3045c46d181b89d87cd47da947e3a4
Deepeshgiri <dr.deepeshgiri@gmail.com>
2024-03-31 14:04:48 +0530

    testing toystack auto deploy

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/message' , messageRoutes);
 app.use('/api/users' , userRoutes);
 
 app.get('/test' , (req, res)=>{
-  res.send(" habibi, I am a happy server!!!")
+  res.send(" habibi, I am a happy server!!!!")
 })
 
 app.listen(PORT, () => {
