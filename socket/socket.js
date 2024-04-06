355527e3d3920a2da8f4aadd660df7cd4791304d
Deepeshgiri <dr.deepeshgiri@gmail.com>
2024-04-06 16:54:57 +0530

    render front address test

--- socket/socket.js
+++ socket/socket.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 const io = new Server(server,
 	 {
 	cors: {
-		origin: ["0.0.0.0/0"],
+		origin: ["https://chatfrontend-346g.onrender.com"],
 		methods: ["GET", "POST"],
 	},
 }
